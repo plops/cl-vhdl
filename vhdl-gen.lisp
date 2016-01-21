5df4aa0543f43b113864f45df31e5bfd6c9f44d9
Martin Kielhorn <kielhorn.martin@gmail.com>
2016-01-21 13:05:08 +0100

    add original vhdl code

--- vhdl-gen.lisp
+++ vhdl-gen.lisp
@@ -1,4 +1,32 @@
+;; -- library declaration
+;; library IEEE;
+;; use IEEE.std_logic_1164.all;
+;; -- entity
+;; entity my_system is
+;;   port ( A,B,C : in std_logic;
+;;          F,Q
+;;          : out std_logic);
+;; end my_system;
+;; -- architecture
+;; architecture behav of my_system is
+;;   signal A1 : std_logic;
+;; begin
+;;   some_proc: process(A,B,C) is
+;;     variable a,b : integer;
+;;   begin
+;;     a:=74;
+;;     b:=67;
+;;     A1 <= A and B and C;
+;;     if a>b then
+;;       F <= A1 or B;
+;;     end if;
+;;   end process some_proc;
+;; -- we are outside the process body
+;;   Q <= not A;
+;; end behav;
+
 (with-vhdl ()
+  ;; example listing 5.4 in free range vhdl
   (library IEEE)
   (use IEEE.std_logic_1164.all)
   (entity my-system
@@ -20,4 +48,5 @@
 				    (sequential-assign a1 (and a b c))
 				    (if (< vb va)
 					(assign f (or a1 b)))))
+		  (comment "we are outside the process body")
 		  (concurrent-assign q (not a)))))
