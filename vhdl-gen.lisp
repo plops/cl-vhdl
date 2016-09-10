c0a50963ea4353a97a9fcc28d45e5d52b5ee3a56
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-09-10 07:07:25 +0200

    first output

--- vhdl-gen.lisp
+++ vhdl-gen.lisp
@@ -6,18 +6,28 @@
 (setf (readtable-case *readtable*) :invert)
 
 (defmacro f (fmt &rest rest)
+  "abbreviation for format nil"
   `(format nil ,fmt ,@rest))
+
+(defmacro c (&body body)
+  "combine multiple outputs into string"
+  `(with-output-to-string (s)
+     (macrolet ((f (fmt &rest rest)
+		  `(format s ,fmt ,@rest)))
+       ,@body)))
 
 (defun emit (code)
   (cond
     ((null code) "")
     ((listp code)
      (case (car code)
-       (entity (destructuring-bind (identitfier header &key (declarative) (simple-name)) (cdr code)))
+       (entity (destructuring-bind (name &key ports) (cdr code)
+		 (c (f "entity ~a is~%" name)
+		    (f "  port(~%~{~a~^~%~});~%" ports)
+		    (f "end ~a;" name))))
        (t (cond ((and (= 2 (length code)) 
 		      (member (car code) '(-))) ;; unary operators
 		 (destructuring-bind (op operand) code
 		   (f "(~a (~a))" op (emit operand))))))))))
 
-;; backus naur for vhdl
-;; http://www.pldworld.net/_hdl/1/www.ireste.fr/fdl/vcl/lesd/Vbnf.htm
+(emit `(entity ckt_e :ports ((ram-cs))))
