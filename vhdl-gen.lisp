e4c3aba4afba4986425aa2b2ebe82d11338c9e98
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-09-10 06:27:49 +0200

    the grammar is huge

--- vhdl-gen.lisp
+++ vhdl-gen.lisp
@@ -13,7 +13,11 @@
     ((null code) "")
     ((listp code)
      (case (car code)
+       (entity (destructuring-bind (identitfier header &key (declarative) (simple-name)) (cdr code)))
        (t (cond ((and (= 2 (length code)) 
 		      (member (car code) '(-))) ;; unary operators
 		 (destructuring-bind (op operand) code
 		   (f "(~a (~a))" op (emit operand))))))))))
+
+;; backus naur for vhdl
+;; http://www.pldworld.net/_hdl/1/www.ireste.fr/fdl/vcl/lesd/Vbnf.htm
