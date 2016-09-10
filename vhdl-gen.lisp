69883421bbb1f776899608dc82e6a36dd4da3a48
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-09-10 05:59:59 +0200

    start outlining the structure

--- vhdl-gen.lisp
+++ vhdl-gen.lisp
@@ -1,11 +1,19 @@
-(defpackage :vhdl-generator
+(defpackage :vhdl
   (:use :cl))
 
-(in-package :vhdl-generator)
+(in-package :vhdl)
 
 (setf (readtable-case *readtable*) :invert)
 
-(defun emit-vhdl (code)
+(defmacro f (fmt &rest rest)
+  `(format nil ,fmt ,@rest))
+
+(defun emit (code)
   (cond
     ((null code) "")
-    (())))
+    ((listp code)
+     (case (car code)
+       (t (cond ((and (= 2 (length code)) 
+		      (member (car code) '(-))) ;; unary operators
+		 (destructuring-bind (op operand) code
+		   (f "(~a (~a))" op (emit operand))))))))))
