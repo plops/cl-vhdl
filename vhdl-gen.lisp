11e24ccfad80affb3c9828dbe8cfd7a463fab188
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-09-11 13:11:01 +0200

    use c and ft to combine strings

--- vhdl-gen.lisp
+++ vhdl-gen.lisp
@@ -9,12 +9,24 @@
   "abbreviation for format nil"
   `(format nil ,fmt ,@rest))
 
+(let ((fmt "~a ~a")
+      (rest '( 1 2 )))
+ `(progn
+    (list 'format 3 'fmt 'rest)))
+
+(defmacro ft (fmd &rest rest)
+  `(format t ,fmd ,@rest))
+
 (defmacro c (&body body)
-  "combine multiple outputs into string"
-  `(with-output-to-string (s)
-     (macrolet ((fs (fmt &rest rest)
-		  `(format s ,fmt ,@rest)))
-       ,@body)))
+  "combine multiple outputs into string, overload standard output so that you can use fs as a abbreviation to (format t ...)"
+  (let ((stream (intern (format nil "~a" (gensym "STREAM")))))
+    `(with-output-to-string (,stream)
+       (let ((*standard-output* ,stream))
+	,@body))))
+
+#+nil
+(c (ft "bla ~a ~a" 3 3)
+   (ft "blub"))
 
 (defun print-type (type)
   (cond
@@ -30,16 +42,20 @@
     ((null code) "")
     ((listp code)
      (case (car code)
+       ;(architecture )
        (entity (destructuring-bind (name &key ports) (cdr code)
-		 (c (fs "entity ~a is~%" name)
-		    (fs "  port(~%~{    ~a~^;~%~});~%" (loop for (name dir type) in ports collect
-						       (f "~a : ~a ~a" name dir (print-type type))))
-		    (fs "end ~a;" name))))
+		 (c (ft "entity ~a is~%" name)
+		    (ft "  port(~%~{    ~a~^;~%~});~%"
+			(loop for (name dir type) in ports collect
+			     (f "~a : ~a ~a" name dir (print-type type))))
+		    (ft "end ~a;" name))))
+      ; (cond-assign (destruc))
        (t (cond ((and (= 2 (length code)) 
 		      (member (car code) '(-))) ;; unary operators
 		 (destructuring-bind (op operand) code
 		   (f "(~a (~a))" op (emit operand))))))))))
 
+#+nil
 (emit `(entity ckt_e :ports ((ram_cs :in std_logic)
 			     (ram_we :in std_logic)
 			     (ram_we :in std_logic)
